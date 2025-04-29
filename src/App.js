7568b2f4565e9967cea84b005c94c7d830ee919c
Roshni0511 <roshnichauhan051@gmail.com>
2025-04-29 16:02:35 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+
 
 
 
 function App() {
   return (
     <>
-      <Home />
+   <Routes>
+      <Route path="/" element={<Home />}/>
+   </Routes>
     </>
   );
 }
