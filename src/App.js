5922816e4aaaf43c01f4d4c5b97e558bcb567197
Vishwa2011 <vishwaitaliya23@gmail.com>
2025-05-05 09:16:54 +0530

    done

--- src/App.js
+++ src/App.js
@@ -33,6 +33,12 @@ import Prvisainquiry from "./pages/Pr-visa-inquiry";
 import Privacypolicy from "./pages/Privacy-policy";
 import TermsAndConditions from "./pages/TermsAndConditions";
 import Declaration from "./pages/Declaration";
+import EmailIcon from '@mui/icons-material/Email';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import StarRateIcon from '@mui/icons-material/StarRate';
 function App() {
   const [loading, setLoading] = useState(true);
   const [showScroll, setShowScroll] = useState(false);
@@ -79,15 +85,73 @@ function App() {
 
   return (
     <>
-      {/* Back to Top Button */}
+      {/* Back to Top Button start*/}
       <div
         className={`xb-backtotop ${showScroll ? "active" : ""}`}
         onClick={scrollToTop}
       >
-        <button className="scroll-btn" style={{background:'#00cc99' ,color:'#fff',padding:'10px 15px',borderRadius:'8px'}}>
+        <button className="scroll-btn" style={{ background: '#00cc99', color: '#fff', padding: '10px 15px', borderRadius: '8px' }}>
           <i className="far fa-arrow-up"></i>
         </button>
       </div>
+      {/* back to top end  */}
+
+    <div>
+        {/* phone start  */}
+        <a href="tel:+917600909090">
+        <button class="btn-floating phone">
+          <LocalPhoneIcon />
+          <span>+91 76 00 90 90 90</span>
+        </button>
+      </a>
+      {/* phone end  */}
+
+      {/* whatsapp start  */}
+      <a href="https://api.whatsapp.com/send?phone=918511110221" target="_blank">
+        <button class="btn-floating whatsapp">
+          <WhatsAppIcon />
+          <span>+91 8511110221</span>
+        </button>
+      </a>
+      {/* whatsapp end  */}
+
+      {/* email start  */}
+      <a href="mailto:goforen@gmail.com" target="_blank">
+        <button class="btn-floating email">
+          <EmailIcon />
+          <span>goforen@gmail.com</span>
+        </button>
+      </a>
+      {/* email end  */}
+
+       {/* facebook start  */}
+       <a href="https://www.facebook.com/go.foren.3" target="_blank">
+        <button class="btn-floating facebook">
+          <FacebookIcon />
+          <span>Facebook</span>
+        </button>
+      </a>
+      {/* facebook end  */}
+
+       {/* Youtube start  */}
+       <a href="https://www.youtube.com/channel/UCL-fnyf8LuCfrqjbCcGLbag" target="_blank">
+        <button class="btn-floating Youtube">
+          <YouTubeIcon />
+          <span>Youtube</span>
+        </button>
+      </a>
+      {/* Youtube end  */}
+
+        {/* rating start  */}
+        <a href="https://search.google.com/local/writereview?placeid=ChIJldkI5_tP4DsRzytjjdBFin4" target="_blank">
+        <button class="btn-floating rating">
+          <StarRateIcon />
+          <span>Youtube</span>
+        </button>
+      </a>
+      {/* rating end  */}
+    </div>
+
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -102,7 +166,7 @@ function App() {
         <Route path="/Work-permit-visa" element={<Workpermitvisa />} />
         <Route path="/Courses" element={<Courses />} />
         <Route path="/IELTS" element={<IELTS />} />
-        
+
         <Route path="/TOEFLIBT" element={<TOEFLIBT />} />
         <Route path="/GRE" element={<GRE />} />
         <Route path="/PTE" element={<PTE />} />
@@ -116,7 +180,7 @@ function App() {
         <Route path="/Contact" element={<Contact />} />
         <Route path="/visitor" element={<Contact />} />
         <Route path="/investor" element={<Contact />} />
-     
+
         <Route path="/Upload-cv" element={<Uploadcv />} />
         <Route path="/Inquiry-association" element={<Inquiryassociation />} />
         <Route path="/Student-visa-inquiry" element={<Studentvisainquiry />} />
