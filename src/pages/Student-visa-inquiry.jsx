6e57b610826a3cbb670460d4ce942b63cfa97672
Roshni0511 <roshnichauhan051@gmail.com>
2025-05-06 17:21:01 +0530

    done

--- src/pages/Student-visa-inquiry.jsx
+++ src/pages/Student-visa-inquiry.jsx
@@ -132,12 +132,12 @@ export default function Studentvisainquiry() {
     setStep((prev) => prev - 1);
   };
   // data-background img start
-  const [background, setBackground] = useState("");
-
-  useEffect(() => {
-    const backgroundUrl = "/assets/pic/breadcrumb-bg.jpg";
-    setBackground(backgroundUrl);
-  }, []);
+     const [background, setBackground] = useState("");
+                      
+                        useEffect(() => {
+                          const backgroundUrl = "/assets/pic/breadcrumb-bg.jpg";
+                          setBackground(backgroundUrl);
+                        }, []);
   // data-background img end
   const [showSpouseDetails, setShowSpouseDetails] = useState(false); // default: No
 
@@ -204,13 +204,30 @@ export default function Studentvisainquiry() {
     <div>
       <Navbar />
       {/* <!-- breadcrumb start --> */}
-      <section
-        className="breadcrumb pos-rel bg_img"
-        style={{ backgroundImage: `url(${background})` }}
-      >
-        <div className="container">
-          <div className="breadcrumb__content">
-            <h2 className="breadcrumb__title" style={{color:'#fff'}}>Student Visa</h2>
+       <section
+  className="breadcrumb pos-rel bg_img"
+  style={{ 
+    backgroundImage: `url(${background})`, 
+    minHeight: '400px',
+    position: 'relative',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  }}
+>
+  {/* Overlay */}
+  <div style={{
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // black overlay with 50% opacity
+    zIndex: 1
+  }}></div>
+
+  <div className="container" style={{ position: 'relative', zIndex: 2 }}>
+    <div className="breadcrumb__content">
+    <h2 className="breadcrumb__title" style={{color:'#fff'}}>Student Visa</h2>
             <ul className="breadcrumb__list clearfix">
               <li className="breadcrumb-item">
                 <a href="/">Home</a>
@@ -218,8 +235,10 @@ export default function Studentvisainquiry() {
 
               <li className="breadcrumb-item">Student Visa</li>
             </ul>
-          </div>
-        </div>
+    </div>
+  </div>
+      
+    
         <div className="breadcrumb__circle">
           <span
             className="big"
